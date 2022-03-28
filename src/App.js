ca3603c3ea9edf5e9dc2f39ed2c307036270a14d
lujohnny95 <lu_johnny95@yahoo.com>
2022-03-28 13:06:58 +0100

    fetch API method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [advice, setAdvice] = useState("");
+  const [adviceID, setAdviceID] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    error: false,
+    message: ""
+  });
+
+  //fetching advice data objects from the API
+  const fetchAdvice = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("https://api.adviceslip.com/advice");
+      if (response.status !== 200) {
+        throw new Error("The API is down, please try again later!");
+      }
+      const data = await response.json();
+      //console.log(data)
+      setAdvice(data.slip.advice);
+      setAdviceID(data.slip.id);
+      setLoading(false);
+    } catch (error) {
+      setError({ error: true, message: error.message });
+    }
+  };
+  
+  if (error.error) {
+    return <h1>{error.message}</h1>
+  }
+
   return (
     <div className="App">
       
