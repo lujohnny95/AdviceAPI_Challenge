5f3f44a73fbf47789fd73eb1d73f3feaafa1ea19
lujohnny95 <lu_johnny95@yahoo.com>
2022-03-28 17:29:43 +0100

    react query fetch API data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 
+const queryClient = new QueryClient()
+
 ReactDOM.render(
-    <App />,
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
   document.getElementById('root')
 );
